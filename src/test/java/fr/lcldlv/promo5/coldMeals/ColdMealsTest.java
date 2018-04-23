b28a2998803570a20ccba08ced8d7d7e217e5044
sara <sara.tahabik@gmail.com>
2018-04-23 11:38:18 +0200

    [ColdMeals] refacto(Count Meals)

--- src/test/java/fr/lcldlv/promo5/coldMeals/ColdMealsTest.java
+++ src/test/java/fr/lcldlv/promo5/coldMeals/ColdMealsTest.java
@@ -8,14 +8,12 @@ public class ColdMealsTest {
     @Test
     public void should_return_0_when_there_is_no_persons_checking_on_thursday_after_21PM() {
 
-        int numberOfColdMeals = new ColdMeals().count();
-        Assertions.assertThat(numberOfColdMeals)
+        Assertions.assertThat(new ColdMeals().count())
                 .isEqualTo(0);
-
     }
 
     @Test
-    public void should_return_0_when_the_checking_date_is_a_thursday_noon() throws Exception {
+    public void should_return_0_when_the_checking_date_is_a_thursday_noon() {
 
         Assertions.assertThat(
                 new ColdMeals(new CheckInDate(12, 00)).count())
