65ae02c7bde6347e6b50c6385bc68233bfcdeb59
gabriel <gabriel.zaafrani@lcdlv.com>
2018-04-23 16:08:48 +0200

    feat(CheckIn): allParticipantsAreLate test

--- src/main/java/fr/lcdlv/promo05/socrates/Planning.java
+++ src/main/java/fr/lcdlv/promo05/socrates/Planning.java
@@ -1,10 +1,12 @@
 package fr.lcdlv.promo05.socrates;
 
+
 import java.util.List;
 
 public class Planning {
 
   public static int countColdMeals(final List<CheckIn> participants) {
-    return 0;
+
+    return participants!=null && participants.size ()>0? participants.size ():0;
   }
 }
