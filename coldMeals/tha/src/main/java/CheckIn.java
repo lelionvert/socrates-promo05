905027e634c8dbc290c90208ca731f8edc7d4b3d
no user <no email>
2018-04-24 15:51:14 +0200

    adding tha version of coldMeals

---
+++ coldMeals/tha/src/main/java/CheckIn.java
@@ -0,0 +1,28 @@
+public class CheckIn {
+
+    private final Integer checkInTime;
+
+    private CheckIn(Integer time) {
+        this.checkInTime = time;
+    }
+
+    public boolean isAfter(int time) {
+        return this.checkInTime >= time;
+    }
+
+    static class CheckInFactory {
+        public static final String CHECKIN_DATE_EMPTY_ERROR_MESSAGE = "Checkin time is empty !";
+        public static final String CHECKIN_DATE_INVALID_ERROR_MESSAGE = "Checkin time is invalid !";
+
+        public static CheckIn createFor(Integer time) {
+            if (time == null) {
+                throw new NullPointerException(CHECKIN_DATE_EMPTY_ERROR_MESSAGE);
+            }
+            if (time < 0 || time > 23) {
+                throw new IllegalArgumentException(CHECKIN_DATE_INVALID_ERROR_MESSAGE);
+            }
+            return new CheckIn(time);
+        }
+    }
+}
+
---
+++ coldMeals/tha/src/main/java/ColdMealsCounter.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class ColdMealsCounter {
+
+    public static final int COLD_MEAL_TIME = 21;
+
+    public static int countColdMeals(List<CheckIn> checkIns) {
+        return Math.toIntExact(checkIns
+            .stream()
+            .filter(checkIn -> checkIn.isAfter(COLD_MEAL_TIME))
+            .count());
+    }
+}
