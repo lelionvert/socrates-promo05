2c8f5e64a7b8475d9808aaa703b11ff5e7e69093
sara <sara.tahabik@gmail.com>
2018-04-24 15:02:59 +0200

    [ColdMeals] refacto(Cold Meals) extracting fields in test file: ColdMealsCounterTest.java

--- coldMeals/dipa_sara/src/test/java/fr/lcldlv/promo5/coldMeals/ColdMealsCounterTest.java
+++ coldMeals/dipa_sara/src/test/java/fr/lcldlv/promo5/coldMeals/ColdMealsCounterTest.java
@@ -9,10 +9,14 @@ import java.util.List;
 
 public class ColdMealsCounterTest {
 
+    private final ColdMealsCounter coldMealsCounter = new ColdMealsCounter();
+    private final CheckInTime checkInTimeBeforeColdMealsLimitTime = new CheckInTime(12);
+    private final CheckInTime checkInTimeAfterColdMealsLimit = new CheckInTime(21);
+
     @Test
     public void should_return_0_if_no_checkIn_after_cold_meal_time_limit() {
 
-        Assertions.assertThat(new ColdMealsCounter().countColdMeals(Arrays.asList()))
+        Assertions.assertThat(coldMealsCounter.countColdMeals(Arrays.asList()))
                 .isEqualTo(0);
     }
 
@@ -20,49 +24,49 @@ public class ColdMealsCounterTest {
     public void should_return_0_if_a_checkIn_before_cold_meal_time_limit() {
 
         Assertions.assertThat(
-                new ColdMealsCounter()
-                        .countColdMeals(Arrays.asList(new CheckInTime(12))))
+                coldMealsCounter
+                        .countColdMeals(Arrays.asList(checkInTimeBeforeColdMealsLimitTime)))
                 .isEqualTo(0);
     }
 
     @Test
     public void should_return_1_if_the_checkIn_is_after_cold_meal_time_limit() {
         Assertions.assertThat(
-                new ColdMealsCounter()
-                        .countColdMeals(Arrays.asList(new CheckInTime(21))))
+                coldMealsCounter
+                        .countColdMeals(Arrays.asList(checkInTimeAfterColdMealsLimit)))
                 .isEqualTo(1);
     }
 
     @Test
     public void should_return_1_if_one_before_and_one_after_cold_meal_time_limit() {
         List<CheckInTime> checkInDates = Lists.newArrayList(
-                new CheckInTime(22),
-                new CheckInTime(20)
+                checkInTimeBeforeColdMealsLimitTime,
+                checkInTimeAfterColdMealsLimit
         );
         Assertions.assertThat(
-                new ColdMealsCounter().countColdMeals(checkInDates))
+                coldMealsCounter.countColdMeals(checkInDates))
                 .isEqualTo(1);
     }
 
     @Test
     public void should_return_2_if_two_checkIns_after_cold_meal_time_limit() {
         List<CheckInTime> checkInDates = Lists.newArrayList(
-                new CheckInTime(22),
-                new CheckInTime(21)
+                checkInTimeAfterColdMealsLimit,
+                checkInTimeAfterColdMealsLimit
         );
         Assertions.assertThat(
-                new ColdMealsCounter().countColdMeals(checkInDates))
+                coldMealsCounter.countColdMeals(checkInDates))
                 .isEqualTo(2);
     }
 
     @Test
     public void should_return_0_if_two_checkIns_before_cold_meal_time_limit() {
         List<CheckInTime> checkInDates = Lists.newArrayList(
-                new CheckInTime(10),
-                new CheckInTime(20)
+                checkInTimeBeforeColdMealsLimitTime,
+                checkInTimeBeforeColdMealsLimitTime
         );
         Assertions.assertThat(
-                new ColdMealsCounter().countColdMeals(checkInDates))
+                coldMealsCounter.countColdMeals(checkInDates))
                 .isEqualTo(0);
     }
 
