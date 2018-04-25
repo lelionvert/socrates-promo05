ce13b458b35b93bc00df3376167a0c5f41eac2c5
no user <no email>
2018-04-25 16:20:27 +0200

    Feat(DietMealsCounter) : return_1_if_1_vegan_and_1_vegetarian_choices

--- src/main/java/fr/lcdlv/promo05/socratesfr/dietmeal/DietMealCounter.java
+++ src/main/java/fr/lcdlv/promo05/socratesfr/dietmeal/DietMealCounter.java
@@ -3,7 +3,16 @@ package fr.lcdlv.promo05.socratesfr.dietmeal;
 import java.util.List;
 
 public class DietMealCounter {
-    public int count(String vegan, List<String> choices) {
+    public int count(String diet, List<String> choices) {
+        if (!choices.isEmpty()) {
+            int count = 0;
+            for (String choice : choices) {
+                if (choice.equals(diet)) {
+                    count++;
+                }
+            }
+            return count;
+        }
         return 0;
     }
 }
--- src/test/java/fr/lcdlv/promo05/socratesfr/dietmeal/DietMealCounterTest.java
+++ src/test/java/fr/lcdlv/promo05/socratesfr/dietmeal/DietMealCounterTest.java
@@ -3,16 +3,32 @@ package fr.lcdlv.promo05.socratesfr.dietmeal;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
 public class DietMealCounterTest {
     @Test
-    public void return_0_if_no_choices() throws Exception {
+    public void return_0_if_no_choices() {
         List<String> choices = new ArrayList<>();
         DietMealCounter dietMealCounter = new DietMealCounter();
         Assertions.assertThat(dietMealCounter.count("VEGAN",choices)).isEqualTo(0);
     }
+    @Test
+    public void return_1_if_1_vegan_choice() {
+        List<String> choices = Collections.singletonList("VEGAN");
+        DietMealCounter dietMealCounter = new DietMealCounter();
+        Assertions.assertThat(dietMealCounter.count("VEGAN",choices)).isEqualTo(1);
+    }
+
+    @Test
+    public void return_1_if_1_vegan_and_1_vegetarian_choices() {
+        DietMealCounter dietMealcounter = new DietMealCounter();
+        List<String> choices = Arrays.asList("VEGETARIAN","VEGAN");
+        Assertions.assertThat(dietMealcounter.count("VEGAN", choices)).isEqualTo(1);
+    }
+
+    
+
+
 }
